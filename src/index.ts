ba48f0b62c65c998dd03827f77b0d797e1335e37
eric-lab-star <cyon256@icloud.com>
2022-08-04 18:33:07 +0900

    color

--- src/index.ts
+++ src/index.ts
@@ -23,17 +23,34 @@ const enemies: Projectile[] = [];
 let animationId: number;
 function animate() {
   animationId = requestAnimationFrame(animate);
-  ctx?.clearRect(0, 0, innerWidth, innerHeight);
+  ctx!.fillStyle = `rgba(0,0,0,0.1)`;
+  ctx!.fillRect(0, 0, innerWidth, innerHeight);
   player1.draw();
-  projectiles.forEach((projectile) => projectile.update());
+  projectiles.forEach((projectile, index) => {
+    projectile.update();
+    //when projectile is outside screen, remove projectile
+    if (
+      projectile.x <= -projectile.radius ||
+      projectile.x >= innerWidth + projectile.radius ||
+      projectile.y <= -projectile.radius ||
+      projectile.y >= innerHeight + projectile.radius
+    ) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    }
+  });
   enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
     const dist = Math.hypot(enemy.x - player1.x, enemy.y - player1.y);
+    //when enemy hits player, end game
     if (dist - enemy.radius - player1.radius <= 0) {
       cancelAnimationFrame(animationId);
     }
+
     projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+      //when projectile hits enemy, remove projectile and enmey
       if (dist - enemy.radius - projectile.radius <= 0) {
         setTimeout(() => {
           enemies.splice(enemyIndex, 1);
@@ -76,14 +93,18 @@ const handleClick = (event: MouseEvent) => {
   const projectile = new Projectile({
     x: Xcenter,
     y: Ycenter,
-    radius: 10,
+    radius: 5,
     color: "blue",
-    velocity: { x: dx, y: dy },
+    velocity: { x: dx * 4, y: dy * 4 },
   });
   projectiles.push(projectile);
 };
 
 addEventListener("click", handleClick);
 
-spawnEnemy();
-animate();
+if (ctx) {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, innerWidth, innerHeight);
+  spawnEnemy();
+  animate();
+}
